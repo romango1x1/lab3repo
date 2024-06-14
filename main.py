e7db51c5e41df3b772988175f6b28e3f3b38851e
UzhvenkoJulia <144743928+UzhvenkoJulia@users.noreply.github.com>
2024-06-15 00:05:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from utils import factorial, is_prime, is_even 
+from utils import factorial, is_prime, is_even, new_function, is_power_of_two
 
 print(factorial(5))
 print(is_prime(5))
 print(is_even(5))
+print(new_function())
+print(is_ppower_of_two(5))
