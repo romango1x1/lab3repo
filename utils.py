af1beeaf1ea71134a62e4694a4c945aa9d55a5b1
ilya <144352232+ilyasudeyko@users.noreply.github.com>
2024-06-14 23:27:35 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,3 +3,12 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+    
